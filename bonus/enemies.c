31836529d54920bf73a3b0dc1f94dd45e1711f21
tsodre-p <tsodre-p@student.42porto.com>
2023-04-14 12:18:57 +0100

    fixed bug when enemies move

--- bonus/enemies.c
+++ bonus/enemies.c
@@ -6,7 +6,7 @@
 /*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 10:52:59 by tsodre-p          #+#    #+#             */
-/*   Updated: 2023/04/13 10:39:53 by tsodre-p         ###   ########.fr       */
+/*   Updated: 2023/04/14 12:17:56 by tsodre-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,12 @@ int	check_next_window(t_stack *stack, int x, int y)
 {
 	if (stack->map_array[y][x] == EMPTY)
 		return (1);
+	if (stack->map_array[y][x] == WALL)
+		return (0);
+	if (stack->map_array[y][x] == COLLECTIBLE)
+		return (0);
+	if (stack->map_array[y][x] == ENEMY)
+		return (0);
 	if (stack->map_array[y][x] == PLAYER)
 	{
 		write(1, "You Died 🔪 Good luck next time!", 35);
@@ -49,7 +55,13 @@ int	enemy_position(t_stack *stack, int random_move)
 		while (j < stack->columns)
 		{
 			if (stack->map_array[i][j] == ENEMY)
+			{
 				enemy_move(stack, j, i, random_move);
+				if (random_move == 3)
+					j++;
+				if (random_move == 2)
+					i++;
+			}
 			j++;
 		}
 		i++;
@@ -65,17 +77,17 @@ void	enemy_move(t_stack *stack, int x, int y, int random_move)
 		if (check_next_window(stack, x, y - 1) == 1)
 			change_enemy_image_w(stack, x, y);
 	}
-	if (random_move == 1)
+	else if (random_move == 1)
 	{
 		if (check_next_window(stack, x - 1, y) == 1)
 			change_enemy_image_a(stack, x, y);
 	}
-	if (random_move == 2)
+	else if (random_move == 2)
 	{
 		if (check_next_window(stack, x, y + 1) == 1)
 			change_enemy_image_s(stack, x, y);
 	}
-	if (random_move == 3)
+	else if (random_move == 3)
 	{
 		if (check_next_window(stack, x + 1, y) == 1)
 			change_enemy_image_d(stack, x, y);
@@ -88,13 +100,13 @@ int	animations(t_stack *stack)
 	static int	i;
 	int			num;
 
-	num = 15000;
+	num = 20000;
 	i++;
 	if (i == num * 2)
 		collectible_animation(stack, 0);
 	if (i == num * 3)
 		random_move(stack);
-	if (i == num * 5)
+	if (i == num * 4)
 	{
 		collectible_animation(stack, 1);
 		i = 0;
